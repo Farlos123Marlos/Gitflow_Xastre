6b1a59807b1fd9c61e2a477e8b66d81074c33827
Farlos123Marlos <92122616+Farlos123Marlos@users.noreply.github.com>
2023-03-08 21:12:55 -0300

    Create Sort.h
    Ordenando e medindo tempo bubble

---
+++ Sort.h
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+class Sort
+{
+
+public:
+
+
+    double BubbleSort(int *A1, int size){
+            int aux;
+            clock_t start = clock();
+            for (int i = 0; i < size - 1; ++i) {
+                for (int j = 0; j < size - i - 1; ++j) {
+                    if (A1[j] > A1[j + 1]) {
+                        aux = A1[j];
+                        A1[j] = A1[j + 1];
+                        A1[j + 1] = aux;
+                    }
+                }
+            }
+            clock_t end = clock();
+            double time = static_cast<double>(end - start) * 1000.0 / CLOCKS_PER_SEC;
+            return time;
+    }
+
+    
+
+
+};
