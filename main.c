520b660606121b12b1780f7526c1a89cc1262cee
Farlos123Marlos <92122616+Farlos123Marlos@users.noreply.github.com>
2023-03-08 21:05:04 -0300

    Update and rename full.c to main.c
    Mudança de Monolítico para orientação à objeto. Arquivo main

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "GenRandom.h"
+#include "Sort.h"
+
+typedef Sort sort;
+using namespace std;
+
+int main() {
+	int **array, *aux,i,j,max = 50000;
+	aux = new int [max];
+	RandomIntArray A(3,max);
+	A.fillRandom(20,2000000);
+	array=A.GetArray();
+    sort Sort;
+
+
+
+    cout << "tempo bubble"<< endl;
+    for(i=0;i<3;i++){
+        for(j=0;j<max;j++){
+            aux[j] = array[i][j];
+        }
+        double time = Sort.BubbleSort(aux, max);
+        cout<<"Array-"<<i<< " ,Execution time of Sort: " << time << " ms" <<endl;
+    }
+
+
+    return 0;
+}
